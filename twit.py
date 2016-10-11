dcda0e8935d7f94627f9df9def4d95cd7d8c7b12
james <james16.foster@gmail.com>
2016-10-11 19:57:35 +0000

    Update

--- twit.py
+++ twit.py
@@ -7,12 +7,16 @@ from requests_oauthlib import OAuth1
 
 
 def getResults(sinceId):
-	url = 'https://api.twitter.com/1.1/search/tweets.json?q=FeesMustFall&src=tyah'+'&'+sinceId+'&result_type=recent'+'&count=50'
-	auth = OAuth1('1WlVtlobestZ0GmbQE1n3hQSM', 'GmARTizEZwmQQtpcl6aVyIdgY4d3iLR4STrqy0XSHxGH1as6K7','784026543232352256-XmIou6ilTZtKs8ef6IBHTJqa2UEtsED', '7G7h3YBrP5EKsIMiPL7DISCKwFgmpeRb852LQumliCkhf')
-	r  = requests.get(url, auth=auth)
-	data = r.json()
-	return data
-
+	from OpenSSL.SSL import SysCallError, ZeroReturnError
+	from requests.exceptions import SSLError
+	try:
+		url = 'https://api.twitter.com/1.1/search/tweets.json?q=FeesMustFall&src=tyah'+'&'+sinceId+'&result_type=recent'+'&count=50'
+		auth = OAuth1('1WlVtlobestZ0GmbQE1n3hQSM', 'GmARTizEZwmQQtpcl6aVyIdgY4d3iLR4STrqy0XSHxGH1as6K7','784026543232352256-XmIou6ilTZtKs8ef6IBHTJqa2UEtsED', '7G7h3YBrP5EKsIMiPL7DISCKwFgmpeRb852LQumliCkhf')
+		r  = requests.get(url, auth=auth)
+		data = r.json()
+		return data
+	except(ZeroReturnError,SSLError):
+		return None
 
 def getLastSinceId():
 	file = loadFromFile()
@@ -25,8 +29,9 @@ def main():
 	while(True):
 		lastSinceId = getLastSinceId() 
 		results = getResults(getLastSinceId())
-		listPrevious = parseResults(results,lastSinceId,listPrevious)
-		time.sleep(60)
+		if(not results == None):
+			listPrevious = parseResults(results,lastSinceId,listPrevious)
+			time.sleep(60)
 
 def loadFromFile():
 	with open("daySave.txt") as file:
@@ -40,20 +45,24 @@ def writeToFile(count,sinceID):
 
 def parseResults(data,sinceID,listPrevious):
 	meta = data['search_metadata']
-	count  = meta['count']
-	sinceId  = meta['max_id']
-	statuses = data['statuses']
-	new = 0
-	ids = []
-	for i in statuses:
-		if(not i['id'] in listPrevious):
-			new = new+1
-		ids.append(i['id'])
+	if(not meta == None):
 
-	listPrevious = ids 
-	print(new)	
-	writeToFile(new,sinceId)
-	return listPrevious
+		count  = meta['count']
+		sinceId  = meta['max_id']
+		statuses = data['statuses']
+		new = 0
+		ids = []
+		for i in statuses:
+			if(not i['id'] in listPrevious):
+				new = new+1
+			ids.append(i['id'])
+
+		listPrevious = ids 
+		print(new)	
+		writeToFile(new,sinceId)
+		return listPrevious
+	else:
+		return listPrevious
 
 	
 main()
